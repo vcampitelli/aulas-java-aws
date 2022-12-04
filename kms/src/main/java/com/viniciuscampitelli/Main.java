9837435651b92687c6d66ae1171f973a5f26e1a4
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2022-12-04 17:12:59 -0300

    :hammer: Adicionando códigos do KMS

--- kms/src/main/java/com/viniciuscampitelli/Main.java
+++ kms/src/main/java/com/viniciuscampitelli/Main.java
@@ -1,6 +1,10 @@
 package com.viniciuscampitelli;
 
 import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.model.DecryptRequest;
+import software.amazon.awssdk.services.kms.model.DecryptResponse;
 
 public class Main {
     // @FIXME forneça o ID da chave recebido
@@ -10,10 +14,19 @@ public class Main {
         // @FIXME forneça o código criptografado recebido
         String ciphertext = "";
 
-        // Esse método converte a string acima na classe SdkBytes necessária para a requisição do SDK
+        Region region = Region.US_EAST_1;
+        KmsClient cliente = KmsClient.builder()
+                .region(region)
+                .build();
+
         SdkBytes sdkBytes = KmsHelper.stringToSdkBytes(ciphertext);
 
-        // @TODO criar KmsClient e invocar o método de descriptografar o código, gerando uma variável chamada "resposta"
+        DecryptRequest requisicao = DecryptRequest.builder()
+                .keyId(keyId)
+                .ciphertextBlob(sdkBytes)
+                .build();
+
+        DecryptResponse resposta = cliente.decrypt(requisicao);
 
         System.out.println(
                 "Texto decifrado: " + KmsHelper.decryptionToString(resposta)
